bc1d5fcecb70dff053f45d1a22218ab4d53b1ddf
Kasper Haugland Nørgaard <khngrd@users.noreply.github.com>
2018-04-16 14:22:57 +0200

    Update khn_reassess_content.js

--- khn_reassess_content.js
+++ khn_reassess_content.js
@@ -1,38 +1,40 @@
-var extWrapper = jQuery("#article, .kb_article");
-jQuery(extWrapper).wrapInner("<div id='extWrapper' style='padding-bottom:30px' />");
+setTimeout(function(){
+    var extWrapper = jQuery("#article, .kb_article");
+    jQuery(extWrapper).wrapInner("<div id='extWrapper' style='padding-bottom:30px' />");
 
-jQuery("img", extWrapper).each(function () {
-    if (jQuery(this).length) {
+    jQuery("img", extWrapper).each(function () {
+        if (jQuery(this).length) {
+            jQuery(this)
+                .attr("href", jQuery(this).attr("src"))
+                .unwrap()
+                .wrap("<a />")
+                .parent()
+                // .attr("href", jQuery(this).attr("src"))
+        }
+    });
+
+    jQuery("a", extWrapper).each(function(index) {
         jQuery(this)
-            .attr("href", jQuery(this).attr("src"))
-            .unwrap()
-            .wrap("<a />")
-            .parent()
-            // .attr("href", jQuery(this).attr("src"))
-    }
-});
+            .nextUntil("a")
+            .andSelf()
+            .wrapAll("<div class='article-custom-section' />")
+    });
 
-jQuery("a", extWrapper).each(function(index) {
-    jQuery(this)
-        .nextUntil("a")
-        .andSelf()
-        .wrapAll("<div class='article-custom-section' />")
-});
-
-jQuery(".article-custom-section:first").addClass("default").css("display", "block");
+    jQuery(".article-custom-section:first").addClass("default").css("display", "block");
 
 
-jQuery("h1", extWrapper).each(function () {
-    // jQuery(this).addClass("accordion-toggle");
-    jQuery(this).first().addClass("accordion-toggle default")
-    jQuery(this).not(":first").addClass("accordion-toggle")
-    jQuery(this).parent().before(this); 
-})
+    jQuery("h1", extWrapper).each(function () {
+        // jQuery(this).addClass("accordion-toggle");
+        jQuery(this).first().addClass("accordion-toggle default")
+        jQuery(this).not(":first").addClass("accordion-toggle")
+        jQuery(this).parent().before(this); 
+    })
 
 
-jQuery(document).ready(function (jQuery) {
-    jQuery('#extWrapper').find('.accordion-toggle').click(function () {
-        jQuery(this).next().slideToggle('fast');
-        jQuery(".article-custom-section").not(jQuery(this).next()).slideUp('fast');
+    jQuery(document).ready(function (jQuery) {
+        jQuery('#extWrapper').find('.accordion-toggle').click(function () {
+            jQuery(this).next().slideToggle('fast');
+            jQuery(".article-custom-section").not(jQuery(this).next()).slideUp('fast');
+        });
     });
 });
