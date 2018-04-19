1ff6f7ccffa5f78c7249b2014d4052a2bb539316
Kasper Haugland Nørgaard <khngrd@users.noreply.github.com>
2018-04-19 10:59:38 +0200

    Update reassess_content_v3_bootstrap.js

--- reassess_content_v3_bootstrap.js
+++ reassess_content_v3_bootstrap.js
@@ -58,9 +58,6 @@ jq2(".panel-heading", extWrapper).each(function () {
     jq2(this).parent().before(this);
 });
 
-/**
- * modal image
- */
 jq2(extWrapper).append("<div class='modal fade' id='imagemodal' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button></div><div class='modal-body'><img src='' id='imagepreview'></div></div></div></div>");
 
 jq2("img", extWrapper).each(function () {
